f723200532d9cffc1080f98524d835e9c61b6dc4
muhammadsandyalanami <elsendy05@gmail.com>
2024-12-05 12:19:25 +0700

    MatematikaCanggih.Java

---
+++ MatematikaCanggih.Java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematika;
+
+/**
+ *
+ * @author ADVAN
+ */
+public class MatematikaCanggih extends Matematika1 {
+    public int modulus(int nilai1, int nilai2){
+        return nilai1 % nilai2;
+    }
+}
