7f607f06b148c406de6ec3a6ba6e75fd887f53fc
muhammadsandyalanami <elsendy05@gmail.com>
2024-12-05 12:17:48 +0700

    Matematika1.java

---
+++ Matematika1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematika;
+
+/**
+ *
+ * @author ADVAN
+ */
+public class Matematika1 {
+      public int pertambahan(int nilai1, int nilai2){
+        return nilai1 + nilai2;
+    }
+    public int perkalian(int nilai1, int nilai2){
+        return nilai1 * nilai2;
+    }
+}
